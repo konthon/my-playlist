a6c2929772905d8fab4ea084ca755b64246d96aa
konthon <konthon@hotmail.co.th>
2022-01-22 04:19:40 +0700

    add smooth scroll, scroll to top when change pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 import PlaylistPage from 'pages/playlist'
 import ArtistPage from 'pages/artist'
@@ -7,6 +7,11 @@ import TrackPage from 'pages/track'
 import AboutPage from 'pages/about'
 
 function App() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scroll({ top: 0, left: 0 })
+  }, [pathname])
+
   return (
     <Routes>
       <Route path='/' element={<PlaylistPage />} />
